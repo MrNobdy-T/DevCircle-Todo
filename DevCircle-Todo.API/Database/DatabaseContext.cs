23dafad43d6231496c45813aec08150b9cee28e9
MrNobdy-T <98278340+MrNobdy-T@users.noreply.github.com>
2024-01-20 17:01:02 +0100

    Started building the base structure of this project

---
+++ DevCircle-Todo.API/Database/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using DevCircle_Todo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevCircle_Todo.API.Database
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<User> Users { get; private set; }
+
+		public DbSet<TodoItem> TodoItems { get; private set;}
+
+        public DatabaseContext()
+        {
+        }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<User>();
+			modelBuilder.Entity<TodoItem>();
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
