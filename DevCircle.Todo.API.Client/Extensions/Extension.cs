e38ca0c56f969005de1c9ebdcefafd664e73cedf
MrNobdy-T <98278340+MrNobdy-T@users.noreply.github.com>
2024-02-05 16:42:55 +0100

    Added project for the API client which is used in the view

---
+++ DevCircle.Todo.API.Client/Extensions/Extension.cs
@@ -0,0 +1,30 @@
+﻿using DevCircle.Todo.API.Client.Services;
+using DevCircle.Todo.Application.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCircle.Todo.API.Client.Extensions
+{
+	public static class Extension
+	{
+		public static IServiceCollection AddAPIServices(this IServiceCollection services)
+		{
+			var config = new ConfigurationBuilder()
+					.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+					.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+					.Build();
+
+			services.AddTransient<AbstractAPIClient, TodoItemService>(x =>
+			{
+				return new TodoItemService(config);
+			});
+
+			return services;
+		}
+	}
+}
---
+++ DevCircle.Todo.API.Client/Services/TodoItemService.cs
@@ -0,0 +1,24 @@
+﻿using DevCircle.Todo.Application.Commands;
+using DevCircle.Todo.Application.Common;
+using Flurl;
+using Flurl.Http;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+
+namespace DevCircle.Todo.API.Client.Services
+{
+	public class TodoItemService : AbstractAPIClient
+	{
+        public TodoItemService(IConfiguration config)
+        {
+			BaseURL = config["API_URI"]!;
+        }
+
+        public async Task<CreateTodoItemResponse> Create(CreateTodoItemRequest request)
+		{
+			var test2 = BaseURL.AppendPathSegment("create");
+			var test =  await BaseURL.AppendPathSegment("create").PostJsonAsync(request).ReceiveJson<CreateTodoItemResponse>();
+			return test;
+		}
+	}
+}
